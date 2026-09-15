cea3967ad91bdb12bb15dc8fdfac97b1d2050c6e
d-plaindoux <d.plaindoux@free.fr>
2026-09-15 21:45:40 +0200

    Format code + 2 minors theorems

--- Mealy/TruthDomain.lean
+++ Mealy/TruthDomain.lean
@@ -5,6 +5,8 @@ import Mathlib.Order.Lattice -- https://leanprover-community.github.io/mathlib4_
 
 namespace TruthDomain
 
+-- Open Question: How this BooleanLattice should be instanciated?
+
 class BooleanLattice (A: Type) [PartialOrder A] where
   bot: A
   top: A
@@ -37,16 +39,17 @@ def le_𝔹₂ : 𝔹₂ → 𝔹₂ → Prop
 
 notation a "⊆₂" b => le_𝔹₂ a b
 
+-- https://leanprover-community.github.io/mathlib4_docs/Mathlib/Tactic/Cases.html
+
 instance PreOrder_𝔹₂: Preorder 𝔹₂ where
-  le := le_𝔹₂
-  le_refl := by intro a ; cases a <;> trivial
+  le       := le_𝔹₂
+  le_refl  := by intro a ; cases a <;> trivial
   le_trans := by intros a b c h1 h2 ; cases a <;> cases b <;> cases c <;> trivial
 
 -- ------------------------------------------------------------------------------------
 -- Partial order for 𝔹₂
 -- ------------------------------------------------------------------------------------
 
--- https://leanprover-community.github.io/mathlib4_docs/Mathlib/Tactic/Cases.html
 instance PartialOrder_𝔹₂: PartialOrder 𝔹₂ where
   __ := PreOrder_𝔹₂
   le_antisymm := by intros a b h1 h2 ; cases a <;> cases b <;> trivial
@@ -58,12 +61,12 @@ instance PartialOrder_𝔹₂: PartialOrder 𝔹₂ where
 -- https://leanprover-community.github.io/mathlib4_docs/Mathlib/Order/Lattice.html#Lattices
 
 def join_𝔹₂ : 𝔹₂ → 𝔹₂ → 𝔹₂
-  | .bot, .bot => .bot
-  | _, _ => .top
+  | .bot, .bot  => .bot
+  | _, _        => .top
 
 def meet_𝔹₂ : 𝔹₂ → 𝔹₂ → 𝔹₂
-  | .top, .top => .top
-  | _, _ => .bot
+  | .top, .top  => .top
+  | _, _        => .bot
 
 instance SemilatticeSup_𝔹₂ : SemilatticeSup 𝔹₂ where
   __ := PartialOrder_𝔹₂
@@ -74,7 +77,7 @@ instance SemilatticeSup_𝔹₂ : SemilatticeSup 𝔹₂ where
 
 instance SemilatticeInf_𝔹₂ : SemilatticeInf 𝔹₂ where
   __ := PartialOrder_𝔹₂
-  inf := meet_𝔹₂
+  inf          := meet_𝔹₂
   inf_le_left  := by intro a b ; cases a <;> cases b <;> trivial
   inf_le_right := by intro a b ; cases a <;> cases b <;> trivial
   le_inf       := by intro a b c h1 h2 ; cases a <;> cases b <;> cases c <;> trivial
@@ -82,6 +85,11 @@ instance SemilatticeInf_𝔹₂ : SemilatticeInf 𝔹₂ where
 instance Lattice_𝔹₂ : Lattice 𝔹₂ where
   __ := SemilatticeInf_𝔹₂
   __ := SemilatticeSup_𝔹₂
+
+-- PartialOrder_𝔹₂ is imported twice ... It should the same
+
+theorem SamePreOrder_𝔹₂ : Lattice_𝔹₂.toPartialOrder = PartialOrder_𝔹₂ :=
+  by rfl
 
 -- ====================================================================================
 -- 𝔹₄
@@ -99,19 +107,19 @@ deriving instance BEq, Hashable for 𝔹₄
 -- ------------------------------------------------------------------------------------
 
 def le_𝔹₄ : 𝔹₄ → 𝔹₄ → Prop
-  | .bot, _ => True
-  | _, .bot => False
-  | .botₚ, _ => True
-  | _, .botₚ => False
-  | .topₚ, _ => True
-  | _, .topₚ  => False
-  | .top, .top => True
+  | .bot, _     => True
+  | _, .bot     => False
+  | .botₚ, _    => True
+  | _, .botₚ    => False
+  | .topₚ, _    => True
+  | _, .topₚ    => False
+  | .top, .top  => True
 
 notation a "⊆₄" b => le_𝔹₄ a b
 
 instance PreOrder_𝔹₄: Preorder 𝔹₄ where
-  le := le_𝔹₄
-  le_refl := by intro a ; cases a <;> trivial
+  le       := le_𝔹₄
+  le_refl  := by intro a ; cases a <;> trivial
   le_trans := by intros a b c h1 h2 ; cases a <;> cases b <;> cases c <;> trivial
 
 -- ------------------------------------------------------------------------------------
@@ -127,33 +135,33 @@ instance PartialOrder_𝔹₄: PartialOrder 𝔹₄ where
 -- ------------------------------------------------------------------------------------
 
 def join_𝔹₄ : 𝔹₄ → 𝔹₄ → 𝔹₄
-  | .bot, p => p
-  | p, .bot => p
-  | .botₚ, p => p
-  | p, .botₚ => p
-  | .topₚ, p => p
-  | p, .topₚ => p
-  | .top, .top => .top
+  | .bot, p     => p
+  | p, .bot     => p
+  | .botₚ, p    => p
+  | p, .botₚ    => p
+  | .topₚ, p    => p
+  | p, .topₚ    => p
+  | .top, .top  => .top
 
 def meet_𝔹₄ : 𝔹₄ → 𝔹₄ → 𝔹₄
-  | .top, p => p
-  | p, .top => p
-  | .topₚ, p => p
-  | p, .topₚ => p
-  | .botₚ, p => p
-  | p, .botₚ => p
-  | .bot, .bot => .bot
+  | .top, p     => p
+  | p, .top     => p
+  | .topₚ, p    => p
+  | p, .topₚ    => p
+  | .botₚ, p    => p
+  | p, .botₚ    => p
+  | .bot, .bot  => .bot
 
 instance SemilatticeSup_𝔹₄ : SemilatticeSup 𝔹₄ where
   __ := PartialOrder_𝔹₄
-  sup := join_𝔹₄
+  sup          := join_𝔹₄
   le_sup_left  := by intro a b ; cases a <;> cases b <;> trivial
   le_sup_right := by intro a b ; cases a <;> cases b <;> trivial
   sup_le       := by intro a b c h1 h2 ; cases a <;> cases b <;> cases c <;> trivial
 
 instance SemilatticeInf_𝔹₄ : SemilatticeInf 𝔹₄ where
   __ := PartialOrder_𝔹₄
-  inf := meet_𝔹₄
+  inf          := meet_𝔹₄
   inf_le_left  := by intro a b ; cases a <;> cases b <;> trivial
   inf_le_right := by intro a b ; cases a <;> cases b <;> trivial
   le_inf       := by intro a b c h1 h2 ; cases a <;> cases b <;> cases c <;> trivial
@@ -161,6 +169,11 @@ instance SemilatticeInf_𝔹₄ : SemilatticeInf 𝔹₄ where
 instance Lattice_𝔹₄ : Lattice 𝔹₄ where
   __ := SemilatticeInf_𝔹₄
   __ := SemilatticeSup_𝔹₄
+
+-- PartialOrder_𝔹₄ is imported twice ... It should the same
+
+theorem SamePreOrder_𝔹₄ : Lattice_𝔹₄.toPartialOrder = PartialOrder_𝔹₄ :=
+  by rfl
 
 -- ====================================================================================
 -- End of File
