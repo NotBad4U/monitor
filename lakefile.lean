50e544b344fb422baa39d58e22a5ba70c98a1087
d-plaindoux <d.plaindoux@free.fr>
2026-09-16 11:28:59 +0200

    Change project configuration from toml to lean

---
+++ lakefile.lean
@@ -0,0 +1,16 @@
+import Lake
+open Lake DSL
+
+package «Mealy» where
+  version := v!"0.1.0"
+
+lean_lib «Mealy»
+
+@[default_target]
+lean_exe «mealy» where
+  root := `Main
+
+require "leanprover-community" / "mathlib"
+
+require «lean-fmt» from git
+  "https://github.com/jcreinhold/lean-fmt" @ "v4.34.0-rc2"
