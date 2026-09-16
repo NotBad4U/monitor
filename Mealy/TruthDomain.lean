02fa6baa8531051da6eafd1e5b3519d5a0a2c338
d-plaindoux <d.plaindoux@free.fr>
2026-09-16 10:09:10 +0200

    Add lean formatter

--- Mealy/TruthDomain.lean
+++ Mealy/TruthDomain.lean
@@ -14,9 +14,11 @@ class BooleanLattice (A : Type) extends Lattice A, BoundedOrder A
   - a ⊓ b: the infimum or meet of a and b
 -/
 
-class BooleanLatticeProperties (A: Type) extends BooleanLattice A where
+class BooleanLatticeProperties (A : Type) extends BooleanLattice A where
   distr_cap : ∀ a b c : A, a ⊓ (b ⊔ c) = (a ⊓ b) ⊔ (a ⊓ c)
   distr_cup : ∀ a b c : A, a ⊔ (b ⊓ c) = (a ⊔ b) ⊓ (a ⊔ c)
+
+namespace TruthDomain_𝔹₂
 
 -- ====================================================================================
 -- 𝔹₂
@@ -27,6 +29,7 @@ class BooleanLatticeProperties (A: Type) extends BooleanLattice A where
 inductive 𝔹₂ where
   | bot
   | top
+
 deriving instance BEq, Hashable for 𝔹₂
 
 -- ------------------------------------------------------------------------------------
@@ -41,18 +44,21 @@ notation a "⊆₂" b => le_𝔹₂ a b
 
 -- https://leanprover-community.github.io/mathlib4_docs/Mathlib/Tactic/Cases.html
 
-instance PreOrder_𝔹₂: Preorder 𝔹₂ where
+instance PreOrder_𝔹₂ : Preorder 𝔹₂ where
   le := le_𝔹₂
-  le_refl := by intro a ; cases a <;> trivial
-  le_trans := by intros a b c h1 h2 ; cases a <;> cases b <;> cases c <;> trivial
+  le_refl := by
+    intro a; cases a <;> trivial
+  le_trans := by
+    intros a b c h1 h2; cases a <;> cases b <;> cases c <;> trivial
 
 -- ------------------------------------------------------------------------------------
 -- Partial order for 𝔹₂
 -- ------------------------------------------------------------------------------------
 
-instance PartialOrder_𝔹₂: PartialOrder 𝔹₂ where
+instance PartialOrder_𝔹₂ : PartialOrder 𝔹₂ where
   __ := PreOrder_𝔹₂
-  le_antisymm := by intros a b h1 h2 ; cases a <;> cases b <;> trivial
+  le_antisymm := by
+    intros a b h1 h2; cases a <;> cases b <;> trivial
 
 -- ------------------------------------------------------------------------------------
 -- Lattice for 𝔹₂
@@ -71,26 +77,30 @@ def meet_𝔹₂ : 𝔹₂ → 𝔹₂ → 𝔹₂
 instance SemilatticeSup_𝔹₂ : SemilatticeSup 𝔹₂ where
   __ := PartialOrder_𝔹₂
   sup := join_𝔹₂
-  le_sup_left := by intro a b ; cases a <;> cases b <;> trivial
-  le_sup_right := by intro a b ; cases a <;> cases b <;> trivial
-  sup_le := by intro a b c h1 h2 ; cases a <;> cases b <;> cases c <;> trivial
+  le_sup_left := by
+    intro a b; cases a <;> cases b <;> trivial
+  le_sup_right := by
+    intro a b; cases a <;> cases b <;> trivial
+  sup_le := by
+    intro a b c h1 h2; cases a <;> cases b <;> cases c <;> trivial
 
 instance SemilatticeInf_𝔹₂ : SemilatticeInf 𝔹₂ where
   __ := PartialOrder_𝔹₂
   inf := meet_𝔹₂
-  inf_le_left := by intro a b ; cases a <;> cases b <;> trivial
-  inf_le_right := by intro a b ; cases a <;> cases b <;> trivial
-  le_inf := by intro a b c h1 h2 ; cases a <;> cases b <;> cases c <;> trivial
+  inf_le_left := by
+    intro a b; cases a <;> cases b <;> trivial
+  inf_le_right := by
+    intro a b; cases a <;> cases b <;> trivial
+  le_inf := by
+    intro a b c h1 h2; cases a <;> cases b <;> cases c <;> trivial
 
 instance Lattice_𝔹₂ : Lattice 𝔹₂ where
   __ := SemilatticeSup_𝔹₂
   __ := SemilatticeInf_𝔹₂
 
-
 -- PartialOrder_𝔹₂ is imported twice ... It should the same
 
-theorem SamePreOrder_𝔹₂ : Lattice_𝔹₂.toPartialOrder = PartialOrder_𝔹₂ :=
-  by rfl
+theorem SamePreOrder_𝔹₂ : Lattice_𝔹₂.toPartialOrder = PartialOrder_𝔹₂ := by rfl
 
 -- ------------------------------------------------------------------------------------
 -- Boolean Lattice for 𝔹₂
@@ -99,8 +109,10 @@ theorem SamePreOrder_𝔹₂ : Lattice_𝔹₂.toPartialOrder = PartialOrder_�
 instance BoundedOrder_𝔹₂ : BoundedOrder 𝔹₂ where
   bot := .bot
   top := .top
-  bot_le := by intro a ; cases a <;> trivial
-  le_top := by intro a ; cases a <;> trivial
+  bot_le := by
+    intro a; cases a <;> trivial
+  le_top := by
+    intro a; cases a <;> trivial
 
 instance BooleanLattice_𝔹₂ : BooleanLattice 𝔹₂ where
   __ := Lattice_𝔹₂
@@ -108,18 +120,25 @@ instance BooleanLattice_𝔹₂ : BooleanLattice 𝔹₂ where
 
 instance BooleanLatticeProperties_𝔹₂ : BooleanLatticeProperties 𝔹₂ where
   __ := BooleanLattice_𝔹₂
-  distr_cap := by intro a b c ; cases a <;> cases b <;> cases c <;> trivial
-  distr_cup := by intro a b c ; cases a <;> cases b <;> cases c <;> trivial
+  distr_cap := by
+    intro a b c; cases a <;> cases b <;> cases c <;> trivial
+  distr_cup := by
+    intro a b c; cases a <;> cases b <;> cases c <;> trivial
+
+end TruthDomain_𝔹₂
 
 -- ====================================================================================
 -- 𝔹₄
 -- ====================================================================================
+
+namespace TruthDomain_𝔹₄
 
 inductive 𝔹₄ where
   | bot
   | botₚ
   | topₚ
   | top
+
 deriving instance BEq, Hashable for 𝔹₄
 
 -- ------------------------------------------------------------------------------------
@@ -137,18 +156,21 @@ def le_𝔹₄ : 𝔹₄ → 𝔹₄ → Prop
 
 notation a "⊆₄" b => le_𝔹₄ a b
 
-instance PreOrder_𝔹₄: Preorder 𝔹₄ where
+instance PreOrder_𝔹₄ : Preorder 𝔹₄ where
   le := le_𝔹₄
-  le_refl := by intro a ; cases a <;> trivial
-  le_trans := by intros a b c h1 h2 ; cases a <;> cases b <;> cases c <;> trivial
+  le_refl := by
+    intro a; cases a <;> trivial
+  le_trans := by
+    intros a b c h1 h2; cases a <;> cases b <;> cases c <;> trivial
 
 -- ------------------------------------------------------------------------------------
 -- Partial order for 𝔹₄
 -- ------------------------------------------------------------------------------------
 
-instance PartialOrder_𝔹₄: PartialOrder 𝔹₄ where
+instance PartialOrder_𝔹₄ : PartialOrder 𝔹₄ where
   __ := PreOrder_𝔹₄
-  le_antisymm := by intros a b h1 h2 ; cases a <;> cases b <;> trivial
+  le_antisymm := by
+    intros a b h1 h2; cases a <;> cases b <;> trivial
 
 -- ------------------------------------------------------------------------------------
 -- Lattice for 𝔹₄
@@ -175,16 +197,22 @@ def meet_𝔹₄ : 𝔹₄ → 𝔹₄ → 𝔹₄
 instance SemilatticeSup_𝔹₄ : SemilatticeSup 𝔹₄ where
   __ := PartialOrder_𝔹₄
   sup := join_𝔹₄
-  le_sup_left := by intro a b ; cases a <;> cases b <;> trivial
-  le_sup_right := by intro a b ; cases a <;> cases b <;> trivial
-  sup_le := by intro a b c h1 h2 ; cases a <;> cases b <;> cases c <;> trivial
+  le_sup_left := by
+    intro a b; cases a <;> cases b <;> trivial
+  le_sup_right := by
+    intro a b; cases a <;> cases b <;> trivial
+  sup_le := by
+    intro a b c h1 h2; cases a <;> cases b <;> cases c <;> trivial
 
 instance SemilatticeInf_𝔹₄ : SemilatticeInf 𝔹₄ where
   __ := PartialOrder_𝔹₄
   inf := meet_𝔹₄
-  inf_le_left := by intro a b ; cases a <;> cases b <;> trivial
-  inf_le_right := by intro a b ; cases a <;> cases b <;> trivial
-  le_inf := by intro a b c h1 h2 ; cases a <;> cases b <;> cases c <;> trivial
+  inf_le_left := by
+    intro a b; cases a <;> cases b <;> trivial
+  inf_le_right := by
+    intro a b; cases a <;> cases b <;> trivial
+  le_inf := by
+    intro a b c h1 h2; cases a <;> cases b <;> cases c <;> trivial
 
 instance Lattice_𝔹₄ : Lattice 𝔹₄ where
   __ := SemilatticeSup_𝔹₄
@@ -192,8 +220,7 @@ instance Lattice_𝔹₄ : Lattice 𝔹₄ where
 
 -- PartialOrder_𝔹₄ is imported twice ... It should the same
 
-theorem SamePreOrder_𝔹₄ : Lattice_𝔹₄.toPartialOrder = PartialOrder_𝔹₄ :=
-  by rfl
+theorem SamePreOrder_𝔹₄ : Lattice_𝔹₄.toPartialOrder = PartialOrder_𝔹₄ := by rfl
 
 -- ------------------------------------------------------------------------------------
 -- Boolean Lattice for 𝔹₄
@@ -202,8 +229,10 @@ theorem SamePreOrder_𝔹₄ : Lattice_𝔹₄.toPartialOrder = PartialOrder_�
 instance BoundedOrder_𝔹₄ : BoundedOrder 𝔹₄ where
   bot := .bot
   top := .top
-  bot_le := by intro a ; cases a <;> trivial
-  le_top := by intro a ; cases a <;> trivial
+  bot_le := by
+    intro a; cases a <;> trivial
+  le_top := by
+    intro a; cases a <;> trivial
 
 instance BooleanLattice_𝔹₄ : BooleanLattice 𝔹₄ where
   __ := Lattice_𝔹₄
@@ -211,9 +240,12 @@ instance BooleanLattice_𝔹₄ : BooleanLattice 𝔹₄ where
 
 instance BooleanLatticeProperties_𝔹₄ : BooleanLatticeProperties 𝔹₄ where
   __ := BooleanLattice_𝔹₄
-  distr_cap := by intro a b c ; cases a <;> cases b <;> cases c <;> trivial
-  distr_cup := by intro a b c ; cases a <;> cases b <;> cases c <;> trivial
+  distr_cap := by
+    intro a b c; cases a <;> cases b <;> cases c <;> trivial
+  distr_cup := by
+    intro a b c; cases a <;> cases b <;> cases c <;> trivial
 
+end TruthDomain_𝔹₄
 -- ====================================================================================
 -- End of File
 -- ====================================================================================
