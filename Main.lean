e5509393986f40877abc99384ded745b5b35dfb1
d-plaindoux <d.plaindoux@free.fr>
2026-09-18 10:04:23 +0200

    Denotational semantic in progress

--- Main.lean
+++
@@ -1,4 +0,0 @@
-import Mealy
-
-def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
--- Mealy/FLTL4.lean
+++ Mealy/FLTL4.lean
@@ -1,3 +1,5 @@
+import Mealy.TruthDomain
+
 namespace FLTL₄
 
 inductive φ : Type (u+1) where
@@ -14,19 +16,29 @@ inductive φ : Type (u+1) where
   | finally : φ → φ
   | globally : φ → φ
 
-prefix:75 "¬" => φ.not
-infixr:130 "∧" => φ.and
-infixr:130 "∨" => φ.or
+prefix:75 "¬ " => φ.not
+infixr:130 " ∧ " => φ.and
+infixr:130 " ∨ " => φ.or
 
-prefix:75 "𝑿" => φ.next
+prefix:75 "𝑿 " => φ.next
 prefix:75 "X̅ " => φ.weak_next -- Je n'arrive pas à combiner \MIX et \overline ou \bar
 
-infixr:110 "𝑼" => φ.until
-infixr:110 "𝑹" => φ.release
+infixr:110 " 𝑼 " => φ.until
+infixr:110 " 𝑹 " => φ.release
 
-prefix:100 "𝑭" => φ.finally
-prefix:100 "𝑮" => φ.globally
+prefix:100 "𝑭 " => φ.finally
+prefix:100 "𝑮 " => φ.globally
 
 notation "{ " e " }" => φ.ap e
 
 example : φ :=  ¬ { 1 } ∧ (𝑭 { 2 })
+
+notation "⟦ " w " ⊨ " p "⟧₄" => (w,p)
+
+open TruthDomain.TruthDomain_𝔹₄ in
+def sem : W × φ → 𝔹₄
+  | ⟦ _ ⊨ .true ⟧₄ => .top
+  | ⟦ _ ⊨ .false ⟧₄ => .top
+  | ⟦ w ⊨ .or φ ψ ⟧₄ => sem ⟦ w ⊨ φ ⟧₄ ⊔ sem ⟦ w ⊨ ψ ⟧₄
+  | ⟦ w ⊨ .and φ ψ ⟧₄ => sem ⟦ w ⊨ φ ⟧₄ ⊓ sem ⟦ w ⊨ ψ ⟧₄
+   |⟦ _ ⊨ _ ⟧₄ => .bot
